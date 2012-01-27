25f76e6b7d674703f90d13e3b4bc34f13e33c56b
Jordan <jmstone617@gmail.com>
2012-01-26 20:22:51 -0500

    added PickyTabBarController classes

---
+++ PickyTabBarController.h
@@ -0,0 +1,16 @@
+//
+//  PickyTabBarController.h
+//
+//  Created by Jordan Stone on 1/26/12.
+//  Copyright (c) 2012 Jordan Stone. All rights reserved.
+//  
+//  This code may be reused for personal or commercial purposes
+//  but comes with no guarentees as to its functionality in all
+//  possible circumstances
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickyTabBarController : UITabBarController
+
+@end
